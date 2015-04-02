a5b64ba4c68b91753f7f131478a3bfd92f8cc97f
Henrique Breim <henrique@breim.com.br>
2015-04-02 18:22:45 -0300

    Add assets/layout/navbar, update redirect to token link on create

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,10 +1,31 @@
 class DocumentsController < ApplicationController
-  def index
-    redirect_to new_document_path
-  end
 
-  def new
-    @document = Document.new
-  end
+	before_action :set_document, only: [:show]
 
+	def index
+		redirect_to new_document_path
+	end
+
+	def new
+		@document = Document.new
+	end
+
+	def create
+		@document = Document.new(document_params)
+		@document.save
+    	redirect_to(document_path(@document.token))
+	end
+
+	def show
+	end
+
+	private
+
+	def set_document
+		@document = Document.find_by_token(params[:id])
+	end
+
+	def document_params
+    	params.require(:document).permit(:body)
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :documents
+	root 'documents#index'
+	resources :documents
 end
