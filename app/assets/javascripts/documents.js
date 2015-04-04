ff1297a1bda7461690ba29318ddc6937179cff0b
Antônio Roberto <forevertonny@gmail.com>
2015-04-03 22:27:38 -0300

    added select with all languages supported by ace
    and adjust to use soft tabs instead tab

--- app/assets/javascripts/documents.js
+++ app/assets/javascripts/documents.js
@@ -1,7 +1,8 @@
 jQuery(function() {
   return $('.source-editor').each(function() {
-    var container, editDiv, editor, editorArea, mode;
+    var container, editDiv, editor, editorArea, mode, selectMode;
     container = $(this);
+    selectMode = $('.source-mode')
     mode = container.data('mode');
     editorArea = container.find('textarea');
     editDiv = $('<div>', {
@@ -10,13 +11,21 @@ jQuery(function() {
       height: editorArea.height(),
       'class': editorArea.attr('class')
     }).insertBefore(editorArea);
+
     editorArea.css('display', 'none');
     editor = ace.edit(editDiv[0]);
     editor.setFontSize('14px');
     editor.getSession().setMode('ace/mode/' + mode);
     editor.getSession().setValue(editorArea.val());
+    editor.setTheme("ace/theme/ambiance");
     editorArea.closest('form').submit(function() {
       editorArea.val(editor.getSession().getValue());
     });
+
+    selectMode.on('change', function(e){
+      console.log('changed');
+      editor.getSession().setMode('ace/mode/' + $(this).val());
+      $('#document_editor_session').val($(this).val())
+    })
   });
 });
